ffb5896264d3b9ce5544771b71724f9425263df8
Andrew_walls1@baylor.edu <Andreww_walls1@baylor.edu>
2024-09-03 15:49:43 -0500

    Addeed print statement

---
+++ MyGraph.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+# python3 -m venv myvenv
+# to activate the ve- source myvenv/bin/activate
+# install 3rd party libraries- pip3 install matplotlib
+plt.show()
